226f7dffc814396caac25a510d3fd62238326de3
sadamccr <sadamccr@gmail.com>
2023-01-19 17:56:48 +0500

    sort nodes added

---
+++ sort.py
@@ -0,0 +1,14 @@
+from main import Node, LinkedList
+
+nn1 = Node(4)
+nn2 = Node(3)
+nn3 = Node(5)
+nn4 = Node(1)
+
+linkedlist = LinkedList()
+linkedlist.insert(nn1)
+linkedlist.insert(nn2)
+linkedlist.insert(nn3)
+linkedlist.insert(nn4)
+
+linkedlist.display()
