39526e44864c89ec12b835388e7815b1230860a9
sadamccr <sadamccr@gmail.com>
2023-01-23 15:49:26 +0500

    sorting done

--- sort.py
+++ sort.py
@@ -1,12 +1,30 @@
 from main import Node, LinkedList
 
 
+def swap_next(linkedlist, previous, largest, next_node):
+    largest.next = next_node.next
+    next_node.next = largest
+    if largest is linkedlist.head:
+        linkedlist.head = next_node
+        return
+    previous.next = next_node
+
+
 def sort(linkedlist):
     # iterations depend on no. of nodes
     iterations = linkedlist.list_length() - 1
     while iterations != 0:
+        largest = linkedlist.head
+        previous = None
+        comparisions = iterations
+        while comparisions != 0:
+            if largest.data > largest.next.data:
+                swap_next(linkedlist, previous, largest, largest.next)
+            else:
+                previous = largest
+                largest = largest.next
+            comparisions -= 1
         iterations -= 1
-        break
 
 
 nn1 = Node(4)
